579100792b81b37dae944fa9c42128140e288a1d
Justin <nwanyajustin@gmail.com>
2019-06-05 12:01:44 +0100

    implemented layout structure

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,8 @@ const adminData = require('./admin');
 router.get(['/','/shops','/shop'], (request, response, next) => {
     const products = adminData.products;
     response.render('shop', {products: products,
-        pageTitle: 'My Shop', path: '/',
+        pageTitle: 'My Shop',
+        path: '/',
         hasProducts: products.length > 0,
         activeShop: true,
         productCSS: true,
